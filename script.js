a1f87c08efef76648d2da1c590e17abbf392ba1c
Paul Lvov <48524294+Merrivoir@users.noreply.github.com>
2022-05-18 16:33:44 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+/* скрытие верхнего меню при прокрутке */
+
+let prevScrollPos = window.scrollY;
+    
+window.onscroll = function (event) {
+  let currentScrollPos = window.scrollY;
+  console.log('начальная позиция: ' + prevScrollPos);
+  console.log('текущая позиция ' + currentScrollPos);
+      
+  if (currentScrollPos > 60 && currentScrollPos > prevScrollPos) {
+    document.querySelector('.top-bar').style.top = "-60px";
+  } else {
+    document.querySelector('.top-bar').style.top = "0";
+  }
+    
+  prevScrollPos = currentScrollPos;
+}
