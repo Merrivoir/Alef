7a64aa4d274dbc2f2b57dd5f3bd6a2a2fdb2af79
Paul Lvov <48524294+Merrivoir@users.noreply.github.com>
2022-05-18 16:43:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let prevScrollPos = window.scrollY;
     
 window.onscroll = function (event) {
   let currentScrollPos = window.scrollY;
-  console.log('начальная позиция: ' + prevScrollPos);
-  console.log('текущая позиция ' + currentScrollPos);
+  /*console.log('начальная позиция: ' + prevScrollPos);
+  console.log('текущая позиция ' + currentScrollPos);*/
       
   if (currentScrollPos > 60 && currentScrollPos > prevScrollPos) {
     document.querySelector('.top-bar').style.top = "-60px";
