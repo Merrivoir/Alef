e3f79732c11eb1f059264a92684458e47eb1dad8
Paul Lvov <48524294+Merrivoir@users.noreply.github.com>
2022-05-22 17:06:57 +0300

    Create index.js

---
+++ JS/index.js
@@ -0,0 +1,45 @@
+var app = new Vue({ 
+    
+    el: '#info',
+    data: {
+        itemname: 'Пижама для мальчиков',
+        sku: '02640141/67',
+        raiting: 4,
+        reviews: 14,
+        price: 1500,
+        currency: '₽',
+        discount: 36,
+        promo: 20
+    },
+    
+    computed: {
+        feedback: function() {
+            let n = Math.abs(this.reviews);
+            n %= 100;      
+            if (n >= 5 && n <= 20) {
+              return this.reviews + ' отзывов';
+            }
+            n %= 10;
+            if (n === 1) {
+              return this.reviews + ' отзыв';
+            }
+            if (n >= 2 && n <= 4) {
+              return this.reviews + ' отзыва';
+            } 
+            return this.reviews + ' отзывов';
+        },
+        
+        nonDiscountPrice: function() {
+            return this.price + ' ' + this.currency;
+        },
+
+        currentPrice: function() {
+            let p = this.price;
+            let d = this.discount;
+            let s = this.promo;
+            p = Math.round((p - p * d/100) / 10) * 10;
+            p = Math.round((p - p * s/100) / 10) * 10;
+            return p + ' ' + this.currency;
+        }
+    }
+});
