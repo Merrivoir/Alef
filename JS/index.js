e54205f7b6332077f00f5cff0eb024d7010377ce
Paul Lvov <48524294+Merrivoir@users.noreply.github.com>
2022-05-26 12:44:37 +0300

    Доработка Vue

--- JS/index.js
+++ JS/index.js
@@ -2,14 +2,14 @@ var app = new Vue({
     
     el: '#info',
     data: {
-        itemname: 'Пижама для мальчиков',
-        sku: '02640141/67',
-        raiting: 4,
-        reviews: 14,
-        price: 1500,
-        currency: '₽',
-        discount: 36,
-        promo: 20
+      itemname: 'Пижама для мальчиков',
+      sku: '02640141/67',
+      raiting: 4,
+      reviews: 14,
+      price: 1500,
+      currency: '₽',
+      discount: 36,
+      promo: 20,
     },
     
     computed: {
@@ -43,3 +43,14 @@ var app = new Vue({
         }
     }
 });
+
+var extra = new Vue({
+  el: '#extra',
+  data: {
+    extraitem_1: 105,
+    extraitem_2: 110,
+    extraitem_3: 120,
+    extraitem_4: 200,
+    extraitem_5: 140,
+  }
+})
